c5c7ffe214de18a0dc4a9e9df810efed568978be
gnzhen <mioyazhen1996@gmail.com>
2018-04-01 17:43:37 +0800

    - QR Scanner

--- app/src/main/java/com/example/gd/oticket/QRCodeActivity.java
+++ app/src/main/java/com/example/gd/oticket/QRCodeActivity.java
@@ -1,22 +1,157 @@
 package com.example.gd.oticket;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.gd.oticket.myrequest.MyRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Iterator;
+
+import me.dm7.barcodescanner.zbar.Result;
+import me.dm7.barcodescanner.zbar.ZBarScannerView;
 
 /**
  * Created by GD on 3/27/2018.
  */
 
-public class QRCodeActivity extends AppCompatActivity {
+public class QRCodeActivity extends AppCompatActivity implements ZBarScannerView.ResultHandler {
+    private ZBarScannerView mScannerView;
+    private static final int MY_CAMERA_REQUEST_CODE = 100;
+    MyRequest request;
+    Toast toast;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_qrcode);
+        mScannerView = new ZBarScannerView(this);    // Programmatically initialize the scanner view
+        setContentView(mScannerView);
+        request = new MyRequest(this);
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.CAMERA},
+                    MY_CAMERA_REQUEST_CODE);
+        }
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
+
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == MY_CAMERA_REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                mScannerView.startCamera();
+            } else {
+                Toast.makeText(this, "camera permission denied", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+            }
+        }
     }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+
+        mScannerView.startCamera();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mScannerView.setResultHandler(this); // Register ourselves as a handler for scan results.
+        mScannerView.resumeCameraPreview(this);// Start camera on resume
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mScannerView.stopCamera();           // Stop camera on pause
+    }
+
+    @Override
+    public void handleResult(Result rawResult) {
+        // Do something with the result here
+        Log.v("qr rawresult", rawResult.getContents()); // Prints scan results
+        Log.v("qr barcodeformat", rawResult.getBarcodeFormat().getName()); // Prints the scan format (qrcode, pdf417 etc.)
+
+        issueTicket(rawResult.getContents());
+        // If you would like to resume scanning, call this method below:
+//        mScannerView.resumeCameraPreview(this);
+    }
+
+    /*
+     * Request functions
+     */
+    public void issueTicket(String branchServiceId) {
+
+        String userId = getApplicationContext().getSharedPreferences("auth", MODE_PRIVATE).getString("id", null);
+        if (userId == null) {
+            showToast("Please login first");
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        }
+        else {
+            /* Issue Ticket */
+            request.issueTicket(userId, branchServiceId, new MyRequest.VolleyCallback() {
+                @Override
+                public void onSuccess(String result) {
+                    try {
+                        JSONObject jsonObject = new JSONObject(result);
+
+                        if (jsonObject.has("success")) {
+                            showToast(jsonObject.get("success").toString());
+
+                        } else if (jsonObject.has("fail")) {
+                            showToast(jsonObject.get("fail").toString());
+                        } else {
+                            //Show first validation error from server
+                            Iterator<String> keys = jsonObject.keys();
+                            String str_Name = keys.next();
+                            JSONArray value;
+                            try {
+                                value = jsonObject.getJSONArray(str_Name);
+                                showToast(value.get(0).toString());
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                    showToast("Ticket issued!");
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                }
+
+                @Override
+                public void onFailure(String error) {
+                    Log.d("onFailure", error);
+                }
+            });
+        }
+    }
+
+    public void showToast(String text) {
+        toast = Toast.makeText(this, text, Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL, 0, 150);
+        toast.show();
+    }
+
 }
